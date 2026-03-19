86b5b6b8b47388695f66c48719649e168a995444
mario <mario_reyes505@hotmail.com>
2026-03-18 20:52:30 -0500

    handle name change with states (first version)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmited] = useState(false);
+
+  const handlePlayerNameChange = (event) => {
+    setSubmited(false);  
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmited(true);  
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handlePlayerNameChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
