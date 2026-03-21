b195c2ceb415bd6a0c08ede6c6deb449772b38ce
mario <mario_reyes505@hotmail.com>
2026-03-20 21:14:59 -0500

    created TimerChallenge.jsx , set up base struct of new comp and add it to App.jsx

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,18 @@
+export const TimerChallenge = ({title, targetTime}) => {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's': ''}
+      </p>
+      <p>
+        <button>
+            Start Challenge
+        </button>
+      </p>
+      <p className="">
+        Time is Running / Time Inactive
+      </p>
+    </section>
+  );
+};
