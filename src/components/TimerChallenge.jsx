28a45cf07e9fbb1b69a7eff8687aa492e6c0eae3
mario <mario_reyes505@hotmail.com>
2026-03-21 20:51:37 -0500

    create ResultModal.jsx to show game result, add it to TimerChallenge.jsx, implement use of refs to show ResultModal when timer ends

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,41 @@
-export const TimerChallenge = ({title, targetTime}) => {
+import { useRef, useState } from "react";
+import { ResultModal } from "./ResultModal";
+
+export const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const dialogRef = useRef();
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const handleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialogRef.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's': ''}
-      </p>
-      <p>
-        <button>
-            Start Challenge
-        </button>
-      </p>
-      <p className="">
-        Time is Running / Time Inactive
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialogRef} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Stop"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is Running..." : "Time Inactive"}
+        </p>
+      </section>
+    </>
   );
 };
